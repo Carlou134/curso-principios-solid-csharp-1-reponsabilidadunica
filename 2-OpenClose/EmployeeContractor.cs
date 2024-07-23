03cd4f77b1da24bfcad37718c37adb3c67d0aac4
Carlos Vásquez Rodriguez <81259501+Carlou134@users.noreply.github.com>
2024-07-23 13:49:30 -0500

    Principios Solid

---
+++ 2-OpenClose/EmployeeContractor.cs
@@ -0,0 +1,19 @@
+namespace OpenClose
+{
+    public class EmployeeContractor : Employee
+    {
+        public EmployeeContractor(string fullname, int hoursWorked)
+        {
+            Fullname = fullname;
+            HoursWorked = hoursWorked;
+        }
+
+        public override decimal CalculateSalaryMonthly()
+        {
+            decimal hourValue = 20000M;
+            decimal salary = hourValue * this.HoursWorked;
+            return salary;
+        }
+
+    }
+}
